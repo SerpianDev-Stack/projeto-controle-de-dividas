fd974d7d20abbb004e7f99e9b9b43bf77ce2586e
SerpianDev-Stack <joaolucasdomingos05@gmail.com>
2026-06-21 21:11:24 -0300

    Funcionalidade: Fluxo do trabalho completo do cliente funcionando

--- src/components/custumerForm/CustumerForm.tsx
+++ src/components/custumerForm/CustumerForm.tsx
@@ -1,15 +1,42 @@
+import { useState } from "react";
+
 type Props = {
   onAddClient: (name: string, value: number) => void;
 };
 
 const CustumerForm = ({ onAddClient }: Props) => {
+  const [name, setName] = useState("");
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    if (!name || !value) return;
+
+    onAddClient(name, Number(value));
+
+    setName("");
+    setValue("");
+  };
+
   return (
     <>
-      <input placeholder="Nome" />
-      <input placeholder="Valor" type="number" />
-      <button>Adicionar</button>
+      <input
+        placeholder="Nome"
+        value={name}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setName(e.target.value)
+        }
+      />
+      <input
+        placeholder="Valor"
+        type="number"
+        value={value}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setValue(e.target.value)
+        }
+      />
+      <button onClick={handleSubmit}>Adicionar</button>
     </>
   );
 };
 
-export default CustumerForm; 
+export default CustumerForm;
